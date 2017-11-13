c9c35a7465cf849e90e63e206c5e9fa344b1e345
AndresV <andresvi94@gmail.com>
2017-11-12 22:57:14 -0600

    Added initContactUsMap method

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -1,17 +1,17 @@
 /*!
-    
+
  =========================================================
  * Material Kit - v1.1.1.0
  =========================================================
- 
+
  * Product Page: http://www.creative-tim.com/product/material-kit
  * Copyright 2017 Creative Tim (http://www.creative-tim.com)
  * Licensed under MIT (https://github.com/timcreative/material-kit/blob/master/LICENSE.md)
- 
+
  =========================================================
- 
+
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  */
 
 var transparent = true;
@@ -107,7 +107,25 @@ materialKitDemo = {
             '-o-transform':'translate3d(0,' + oVal +'px,0)'
         });
 
-    }, 6)
+    }, 6),
+    initContactUsMap: function(){
+         var myLatlng = new google.maps.LatLng(32.7897391, -79.9383071);
+         var mapOptions = {
+           zoom: 14,
+           center: myLatlng,
+           styles:
+ [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
+           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+         }
+         var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+         var marker = new google.maps.Marker({
+             position: myLatlng,
+             title:"Hello World!"
+         });
+         marker.setMap(map);
+     }
+
+
 
 }
 // Returns a function, that, as long as it continues to be invoked, will not
